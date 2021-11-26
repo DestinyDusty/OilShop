a0788acab30620635198ef407cb48b1d0dc8075a
Dusty <egor.goldobin2016yandex.ru@gmail.com>
2021-11-26 15:22:54 +0500

    Управление пользователями

---
+++ OilShop/Controllers/BrandsController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OilShop.Models;
+using OilShop.Models.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OilShop.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly AppCtx _context;
+
+        public BrandsController(AppCtx context)
+        {
+            _context = context;
+        }
+
+        // GET: Brands
+        public async Task<IActionResult> Index()
+        {
+            var appCtx = _context.Brands.Include(b => b.User);
+            return View(await appCtx.ToListAsync());
+        }
+
+        // GET: Brands/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // GET: Brands/Create
+        public IActionResult Create()
+        {
+            ViewData["IdUser"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Brands/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,BrandOil,IdUser")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUser"] = new SelectList(_context.Users, "Id", "Id", brand.IdUser);
+            return View(brand);
+        }
+
+        // GET: Brands/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdUser"] = new SelectList(_context.Users, "Id", "Id", brand.IdUser);
+            return View(brand);
+        }
+
+        // POST: Brands/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("Id,BrandOil,IdUser")] Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(brand);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandExists(brand.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUser"] = new SelectList(_context.Users, "Id", "Id", brand.IdUser);
+            return View(brand);
+        }
+
+        // GET: Brands/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // POST: Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandExists(short id)
+        {
+            return _context.Brands.Any(e => e.Id == id);
+        }
+    }
+}
